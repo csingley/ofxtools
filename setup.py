0518a77c74570eead91704c2c812b0af933d41b6
Christopher Singley <csingley@gmail.com>
2015-02-08 21:52:15 -0600

    Modified setup.py for PyPI upload

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from distutils.core import setup
 from codecs import open # To use a consistent encoding
 from os import path
 
@@ -10,36 +11,44 @@ with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='ofxtools',
-    version='0.1',
-    description=('Library for downloading and parsing Open Financial Exchange'
-                 '(OFX) formatted data from financial institutions'),
-    long_description=long_description,
+    name = 'ofxtools',
+    packages = ['ofxtools'],
+    version = '0.1',
+    description = ('Library for working with Open Financial Exchange (OFX)'
+                 'formatted data used by financial institutions'),
+    long_description = long_description,
 
-    url='https://github.com/csingley/ofxtools',
+    url = 'https://github.com/csingley/ofxtools',
+    download_url = 'https://github.com/csingley/ofxtools/tarball/0.1',
 
-    author='Christopher Singley',
-    author_email='csingley@gmail.com',
+    author = 'Christopher Singley',
+    author_email = 'csingley@gmail.com',
 
-    license='MIT',
+    license = 'MIT',
 
-    classifiers=[
+    classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
+        'Topic :: Office/Business :: Financial :: Accounting',
+        'Topic :: Office/Business :: Financial :: Investment',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English', 
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
 
-    keywords='ofx, Open Financial Exchange',
-
-    packages=['ofx'],
+    keywords = ['ofx', 'Open Financial Exchange'],
 
     data_files=[
         ('config', ['config/fi.cfg']),
