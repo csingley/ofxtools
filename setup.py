b4fb5024daab7746023edad8f74e7af781fe5a36
Christopher Singley <csingley@gmail.com>
2015-02-25 12:14:25 -0600

    Fix setup.py typos

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('README', 'r') as f:
     
 setup(
     name = 'ofxtools',
-    version = '0.3.2',
+    version = '0.3.3',
     description = ('Library for working with Open Financial Exchange (OFX) '
                  'formatted data used by financial institutions'),
     long_description = long_description,
 
     url = 'https://github.com/csingley/ofxtools',
-    download_url = 'https://github.com/csingley/ofxtools/tarball/0.3.2',
+    download_url = 'https://github.com/csingley/ofxtools/tarball/0.3.3',
 
     author = 'Christopher Singley',
     author_email = 'csingley@gmail.com',
@@ -58,7 +58,7 @@ setup(
 
     package_data = {
         'ofxtools': ['config/*.cfg', 'tests/*'],
-    }
+    },
 
     entry_points = {
         'console_scripts': [
