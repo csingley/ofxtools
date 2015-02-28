82b2030e1e90b86d56b4bbf2fbecc39234f06fc1
Christopher Singley <csingley@gmail.com>
2015-02-27 21:54:51 -0600

    Release 0.3.7

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README', 'r') as f:
     
 setup(
     name = 'ofxtools',
-    version = '0.3.6',
+    version = '0.3.7',
     description = ('Library for working with Open Financial Exchange (OFX) '
                  'formatted data used by financial institutions'),
     long_description = long_description,
 
     url = 'https://github.com/csingley/ofxtools',
-    download_url = 'https://github.com/csingley/ofxtools/tarball/0.3.6',
+    download_url = 'https://github.com/csingley/ofxtools/tarball/0.3.7',
 
     author = 'Christopher Singley',
     author_email = 'csingley@gmail.com',
