5daf33c45734e0f1657bfe8fe602cdc9d192f25a
Christopher Singley <csingley@gmail.com>
2017-04-07 00:07:51 -0500

    ofx.models - refactor accounts/balances out of base

---
+++ ofxtools/models/accounts.py
@@ -0,0 +1,48 @@
+# vim: set fileencoding=utf-8
+"""
+Python object model for fundamental data aggregates such as transactions,
+balances, and securities.
+"""
+# local imports
+from ofxtools.models import (
+    Aggregate,
+    ACCTTYPES,
+)
+from ofxtools.Types import (
+    String,
+    OneOf,
+)
+
+
+class ACCTFROM(Aggregate):
+    """ Base class (not in OFX spec) for *ACCTFROM/*ACCTTO """
+    acctid = String(22, required=True)
+
+
+class BANKACCTFROM(ACCTFROM):
+    """ OFX section 11.3.1 """
+    bankid = String(9, required=True)
+    branchid = String(22)
+    accttype = OneOf(*ACCTTYPES,
+                     required=True)
+    acctkey = String(22)
+
+
+class BANKACCTTO(BANKACCTFROM):
+    """ OFX section 11.3.1 """
+    pass
+
+
+class CCACCTFROM(ACCTFROM):
+    """ OFX section 11.3.2 """
+    acctkey = String(22)
+
+
+class CCACCTTO(CCACCTFROM):
+    """ OFX section 11.3.2 """
+    pass
+
+
+class INVACCTFROM(ACCTFROM):
+    """ """
+    brokerid = String(22, required=True)
---
+++ ofxtools/models/balances.py
@@ -0,0 +1,47 @@
+# vim: set fileencoding=utf-8
+"""
+Python object model for fundamental data aggregates such as transactions,
+balances, and securities.
+"""
+# local imports
+from ofxtools.models import (
+    Aggregate,
+    CURRENCY,
+)
+from ofxtools.Types import (
+    String,
+    OneOf,
+    Decimal,
+    DateTime,
+)
+
+
+class LEDGERBAL(Aggregate):
+    """ """
+    balamt = Decimal(required=True)
+    dtasof = DateTime(required=True)
+
+
+class AVAILBAL(Aggregate):
+    """ """
+    balamt = Decimal(required=True)
+    dtasof = DateTime(required=True)
+
+
+class INVBAL(Aggregate):
+    """ OFX section 13.9.2.7 """
+    availcash = Decimal(required=True)
+    marginbalance = Decimal(required=True)
+    shortbalance = Decimal(required=True)
+    buypower = Decimal()
+
+    _subaggregates = ('BALLIST',)
+
+
+class BAL(CURRENCY):
+    """ OFX section 3.1.4 """
+    name = String(32, required=True)
+    desc = String(80, required=True)
+    baltype = OneOf('DOLLAR', 'PERCENT', 'NUMBER', required=True)
+    value = Decimal(required=True)
+    dtasof = DateTime()
