b2b0260b9386dff91f2d7dc47b1abd3bfdff68a6
Christopher Singley <csingley@gmail.com>
2019-03-21 10:48:58 -0500

    Prepare for 0.5.4 release

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Christopher Singley'
 author = 'Christopher Singley'
 
 # The short X.Y version
-version = '0.5.3'
+version = '0.5.4'
 # The full version, including alpha/beta/rc tags
-release = '0.5.3'
+release = '0.5.4'
 
 
 # -- General configuration ---------------------------------------------------
