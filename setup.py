02ef15cfd9e0fadcd45a3d365530a6d703933f84
Christopher Singley <csingley@gmail.com>
2019-03-21 05:29:29 -0500

    Prep for 0.5.3 release

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ with open(os.path.join(__here__, 'README.rst'), 'r') as f:
 
 setup(
     name='ofxtools',
-    version='0.5.2',
+    version='0.5.3',
     # Note: change 'master' to the tag name when release a new verion
-    download_url='https://github.com/csingley/ofxtools/tarball/0.5.2',
+    download_url='https://github.com/csingley/ofxtools/tarball/0.5.3',
 
     description=('Library for working with Open Financial Exchange (OFX) '
                  'formatted data used by financial institutions'),
@@ -46,6 +46,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     keywords=['ofx', 'Open Financial Exchange'],
