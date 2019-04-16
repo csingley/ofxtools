f199fc5d6e1f26783a58dbb6e851dd7c9ad3f8c3
Christopher Singley <csingley@gmail.com>
2019-04-16 09:28:58 -0500

    Fix setup.py for 0.7.2 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os.path
 __here__ = os.path.dirname(os.path.realpath(__file__))
 
 about = {}
-with open(os.path.join(__here__, 'ofxtools', '__version__.py'), 'r', 'utf-8') as f:
+with open(os.path.join(__here__, 'ofxtools', '__version__.py'), 'r') as f:
     exec(f.read(), about)
 
 with open(os.path.join(__here__, 'README.rst'), 'r') as f:
@@ -18,17 +18,17 @@ url_base = "{}/tarball".format(about["__url__"]),
 
 setup(
     name=about["__title__"],
-    version=about["__version"],
-    description=about["__description"],
+    version=about["__version__"],
+    description=about["__description__"],
     long_description=readme,
     long_description_content_type="text/x-rst",
     author=about["__author__"],
-    author_email=about["__author_url__"],
+    author_email=about["__author_email__"],
     url=about["__url__"],
     packages=find_packages(),
     package_data={'ofxtools': ['README.rst', 'config/*.cfg', 'tests/*']},
     python_requires=">=3.4",
-    license=about["__license"],
+    license=about["__license__"],
 
     # Note: change 'master' to the tag name when releasing a new verion
     #  download_url="{}/master".format(url_base),
@@ -56,6 +56,5 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-
     keywords=['ofx', 'Open Financial Exchange'],
 )
