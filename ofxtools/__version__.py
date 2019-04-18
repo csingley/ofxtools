49bdf7cdd6a07de64838e26b365177a328cd0af6
Christopher Singley <csingley@gmail.com>
2019-04-18 08:49:38 -0500

    Prepare for release 0.7.4

--- ofxtools/__version__.py
+++ ofxtools/__version__.py
@@ -1,7 +1,12 @@
+"""
+TAKE A LOOK AT THESE HANDS
+No thanks
+I'm a government man
+"""
 __title__ = "ofxtools"
 __description__ = "Library for working with Open Financial Exchange (OFX) data"
 __url__ = "https://github.com/csingley/ofxtools"
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 __author__ = "Christopher Singley"
 __author_email__ = "csingley@gmail.com"
 __license__ = "MIT"
