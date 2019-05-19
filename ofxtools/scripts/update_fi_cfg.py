544054cc313a4d762ce3b308e5621bdbd17d800d
Christopher Singley <csingley@gmail.com>
2019-05-19 15:02:13 -0500

    update_fi_cfg.py - include stub cfg for nonresponsive FIs

--- ofxtools/scripts/update_fi_cfg.py
+++ ofxtools/scripts/update_fi_cfg.py
@@ -2,8 +2,12 @@
 # coding: utf-8
 """
 Perform an OFX profile scan for the entire OFX Home list of FIs;
-write working connection parameters to user config file with the
-OFX Home id# as the server nickname
+update config/fi.cfg
+
+You probably don't want to run this script; it's for the library developers.
+
+It takes a long time to run and generates lots of HTTP requests, and the output
+is not at all stable so it needs to be checked.
 """
 
 
@@ -12,7 +16,7 @@ import configparser
 from configparser import ConfigParser
 from collections import ChainMap
 from xml.sax import saxutils
-from typing import Mapping, MutableMapping, Optional
+from typing import Mapping, Optional
 
 
 # local imports
@@ -69,10 +73,7 @@ def main():
         print("Scanning {}".format(ofxhome_id))
 
         lookup: Optional[ofxhome.OFXServer] = ofxhome.lookup(ofxhome_id)
-        if lookup is None\
-           or lookup.url is None\
-           or ofxhome.ofx_invalid(lookup)\
-           or ofxhome.ssl_invalid(lookup):
+        if lookup is None or lookup.url is None:
             continue
 
         url = lookup.url
@@ -88,11 +89,15 @@ def main():
         if ofxhome_id not in names:
             names[ofxhome_id] = lookup_name
 
-        scan_results: ofxget.ScanResults = ofxget._scan_profile(
-            url=url, org=org, fid=fid, timeout=10.0
-        )
+        if ofxhome.ofx_invalid(lookup) or ofxhome.ssl_invalid(lookup):
+            blank_fmt = {"versions": [], "formats": {}}
+            scan_results = (blank_fmt, blank_fmt, {})
+        else:
+            scan_results: ofxget.ScanResults = ofxget._scan_profile(
+                url=url, org=org, fid=fid, timeout=10.0
+            )
 
-        v1, v2, signoninfo = scan_results
+        v1, v2, _ = scan_results
         if (not v2["versions"]) and (not v1["versions"]):
             # If no OFX response, blank the server config
             LibraryConfig[srvr_nick] = {"ofxhome": ofxhome_id}
