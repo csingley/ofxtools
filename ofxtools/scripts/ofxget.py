5043f4c82f4fd435fd826445eef766ee63e13650
Chris Singley <csingley@gmail.com>
2026-06-12 11:35:14 -0700

    Move __future__ import to correct position in ofxget.py

--- ofxtools/scripts/ofxget.py
+++ ofxtools/scripts/ofxget.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from __future__ import annotations
 """
 Configurable CLI front end for ``ofxtools.Client``
 """
 
 # stdlib imports
+from __future__ import annotations
+
 import argparse
 import concurrent.futures
 import configparser
